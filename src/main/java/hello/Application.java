74a64ab180193fd83d706eab4b31d1196a083ae6
mmartinezespichan <mmartinezespichan@yahoo.com>
2021-01-30 14:03:35 -0500

    Update Application.java

--- src/main/java/hello/Application.java
+++ src/main/java/hello/Application.java
@@ -13,7 +13,7 @@ public class Application {
     public String home() {
         return "<html><boby>"
         		+ "<h2>Hello, World! from Trunkbase v.1.0</h2><br /><br /><br />"      
-        		+ "<img src=\"/hello.png\">"  		
+        		+ "<img src=\"/hello.jpg\">"  		
         		+ "</boby></html>";
     }
 
